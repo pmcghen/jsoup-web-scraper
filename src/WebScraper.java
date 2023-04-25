c0e2b1bdddd68130ca5ff4d2123b5a75db424612
Pat McGhen <patrickmcghen@gmail.com>
2023-04-25 09:21:29 -0400

    Add test case for looking to the previous month.

--- src/WebScraper.java
+++ src/WebScraper.java
@@ -55,27 +55,16 @@ public class WebScraper {
         System.out.println(count + " jobs were posted on " + url + " in the last 7 days");
     }
 
-    /**
-     * Takes the datetime attribute of a job post and returns whether that date occurs in the last 7 days.
-     *
-     * @param datestamp a string containing the value from the datetime attribute from an HTML time element
-     * @return boolean
-     */
-    public static boolean wasPostedWithinThreshold(String datestamp) {
+    public static boolean wasPostedWithinThreshold(String datestamp, int currentMonth, int currentDate) {
         if (datestamp.length() > 0) {
+            int prevMonth = currentMonth - 1;
             int postMonth = Integer.parseInt(datestamp.substring(5, 7));
             int postDay = Integer.parseInt(datestamp.substring(8, 10));
 
-            ZoneId zonedId = ZoneId.of( "America/New_York" );
-            LocalDate today = LocalDate.now( zonedId );
-            int thisMonth = today.getMonthValue();
-            int todaysDate = today.getDayOfMonth();
-            int prevMonth = thisMonth - 1;
-
-            if (postMonth == thisMonth && postDay >= todaysDate - 7) {
+            if (postMonth == currentMonth && postDay >= currentDate - 7) {
                 return true;
-            } else if (postMonth == prevMonth && todaysDate - 7 < 1) {
-                int dayDifferential = todaysDate - 7;
+            } else if (postMonth == prevMonth && currentDate - 7 < 1) {
+                int dayDifferential = currentDate - 7;
                 int lastDayOfMonth = 0;
                 int[] longerMonths = {1, 3, 5, 7, 8, 10, 12};
                 int[] standardMonths = {4, 6, 9, 11};
@@ -84,19 +73,35 @@ public class WebScraper {
 
                 if (prevMonth == 2) {
                     lastDayOfMonth = 28;
-                }
-
-                if (longerMonthsIndex > 0) {
+                } else if (longerMonthsIndex > 0) {
                     lastDayOfMonth = 31;
-                }
-
-                if (standardMonthsIndex > 0) {
+                } else if (standardMonthsIndex > 0) {
                     lastDayOfMonth = 30;
                 }
 
-                return postDay >= (lastDayOfMonth - dayDifferential);
+                return postDay >= (lastDayOfMonth + dayDifferential);
             }
         }
+
+        return false;
+    }
+
+    /**
+     * Takes the datetime attribute of a job post and returns whether that date occurs in the last 7 days.
+     *
+     * @param datestamp a string containing the value from the datetime attribute from an HTML time element
+     * @return boolean
+     */
+    public static boolean wasPostedWithinThreshold(String datestamp) {
+        if (datestamp.length() > 0) {
+            ZoneId zonedId = ZoneId.of("America/New_York");
+            LocalDate today = LocalDate.now(zonedId);
+            int thisMonth = today.getMonthValue();
+            int todaysDate = today.getDayOfMonth();
+
+            return wasPostedWithinThreshold(datestamp, thisMonth, todaysDate);
+        }
+
         return false;
     }
 }
--- src/WebScraperTest.java
+++ src/WebScraperTest.java
@@ -1,3 +1,4 @@
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -5,7 +6,9 @@ import static org.junit.jupiter.api.Assertions.*;
 class WebScraperTest {
 
     @Test
+    @DisplayName("Check that date falls within threshold.")
     void wasPostedWithinThreshold() {
         assertFalse(WebScraper.wasPostedWithinThreshold("2023-03-31T14:37:49Z"));
+        assertTrue(WebScraper.wasPostedWithinThreshold("2023-03-31T14:37:49Z", 4, 1));
     }
 }
